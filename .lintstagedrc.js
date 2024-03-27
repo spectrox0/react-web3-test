beec61447c904026be912cc2688f61edab168940
Alejandro Velazco <alejanvelazco2008@proton.me>
2024-03-27 01:48:51 -0300

    fix: problem with cypress

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,22 @@
 export default {
-  // Type check TypeScript files
-  "**/*.(ts|tsx)": () => "pnpm tsc --noEmit",
+  // Type check TypeScript files, excluding cy.ts
+  "**/*.(ts|tsx)": filenames => {
+    const filteredFilenames = filenames.filter(
+      name => !name.includes("cy.ts") && !name.includes("cy.js")
+    );
+    return `pnpm tsc --noEmit --files ${filteredFilenames.join(" ")}`;
+  },
 
-  // Lint & Prettify TS and JS files
-  "**/*.(ts|tsx|js)": filenames => [
-    `pnpm eslint --fix ${filenames.join(" ")}`,
-    `pnpm prettier --write ${filenames.join(" ")}`,
-  ],
+  // Lint & Prettify TS and JS files, excluding cy.ts and cy.js
+  "**/*.(ts|tsx|js)": filenames => {
+    const filteredFilenames = filenames.filter(
+      name => !name.includes("cy.ts") && !name.includes("cy.js")
+    );
+    return [
+      `pnpm eslint --fix ${filteredFilenames.join(" ")}`,
+      `pnpm prettier --write ${filteredFilenames.join(" ")}`,
+    ];
+  },
 
   // Prettify only Markdown and JSON files
   "**/*.(md|json)": filenames => `pnpm prettier --write ${filenames.join(" ")}`,
