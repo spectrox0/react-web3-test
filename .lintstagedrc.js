9b6d3252f76088c99dd285c46083befc10c58b79
Alejandro Velazco <alejanvelazco2008@proton.me>
2024-03-31 18:50:14 -0300

    fix: :test_tube:
    Include tests with synpress

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,23 +1,18 @@
 export default {
-  // Type check TypeScript files, excluding cy.ts and cy.js
-  "**/*.(ts|tsx)": filenames => {
-    const filteredFilenames = filenames.filter(
-      name => !/cy\.(ts|js)$/.test(name)
-    );
-    return `pnpm tsc --noEmit --files ${filteredFilenames.join(" ")}`;
-  },
+  "**/*.(ts|tsx)": () => "pnpm tsc --noEmit",
 
-  // Lint & Prettify TS and JS files, excluding cy.ts and cy.js
   "**/*.(ts|tsx|js)": filenames => {
     const filteredFilenames = filenames.filter(
-      name => !/cy\.(ts|js)$/.test(name)
+      name =>
+        !/cy\.(ts|js|tsx)$/.test(name) &&
+        !name.includes("node_modules/@synthetixio/synpress")
     );
+
     return [
       `pnpm eslint --fix ${filteredFilenames.join(" ")}`,
       `pnpm prettier --write ${filteredFilenames.join(" ")}`,
     ];
   },
 
-  // Prettify only Markdown and JSON files
   "**/*.(md|json)": filenames => `pnpm prettier --write ${filenames.join(" ")}`,
 };
